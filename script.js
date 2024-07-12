d6f10eaaabd9b18484e5fe6f41200f3bc0194dd5
jrc03 <joserdlc2727@gmail.com>
2024-07-12 14:08:28 -0400

    All done
    just remain to complete the rgb button, rn its just rbg or solid.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const container = document.querySelector(".container");
 const setGridButton = document.getElementById("setGrid");
 const gridSizeInput = document.getElementById("gridSize");
+const resetGridButton= document.getElementById("resetGrid");
+const rgbButton= document.getElementById("rgbHover")
+
+
+resetGridButton.addEventListener("click", () => {
+    const gridSize = parseInt(gridSizeInput.value);
+    createGrid(gridSize); // Reset the grid to 16x16 squares
+});
 
 setGridButton.addEventListener("click", () => {
     const gridSize = parseInt(gridSizeInput.value);
     createGrid(gridSize);
+   
 });
 
 function createGrid(size) {
@@ -21,6 +30,21 @@ function createGrid(size) {
         div.style.height = `${25}px`;
         container.appendChild(div);
     }
+}
+
+
+function hoverEffect(){
+
+    const gridItems = document.querySelectorAll('.grid-item');
+    
+   gridItems.forEach(item =>{
+        item.addEventListener('mouseover', function(){
+            const r = "#"+Math.floor(Math.random()*16777215).toString(16);
+            item.style.backgroundColor = r;
+        })
+
+   })
+    
 }
 
 
